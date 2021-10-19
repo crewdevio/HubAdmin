8d50c32b812052be21be1d7f39ad30c9632387d4
Cryp18 <ajreyes@intellisys.com.do>
2021-10-19 12:17:25 -0400

    fix: bug validate args

--- utils/ValidateArgs.go
+++ utils/ValidateArgs.go
@@ -5,29 +5,35 @@ import (
 )
 
 func ValidateArgs() {
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
 
-	switch os.Args[1] {
+		case "add":
+			if len(os.Args) < 3 {
+				PrintUsage()
+				os.Exit(1)
+			}
+		case "use":
+			if len(os.Args) < 2 {
+				PrintUsage()
+				os.Exit(1)
+			}
+		case "delete":
+			if len(os.Args) < 2 {
+				PrintUsage()
+				os.Exit(1)
+			}
+		case "list":
 
-	case "add":
-		if len(os.Args) < 3 {
+		case "help":
+			PrintUsage()
+
+		default:
 			PrintUsage()
 			os.Exit(1)
 		}
-	case "use":
-		if len(os.Args) < 2 {
-			PrintUsage()
-			os.Exit(1)
-		}
-	case "delete":
-		if len(os.Args) < 2 {
-			PrintUsage()
-			os.Exit(1)
-		}
-	case "list":
-
-	default:
+	} else {
 		PrintUsage()
-		os.Exit(1)
 	}
 
 }
